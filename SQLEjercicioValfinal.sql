ea652e17ef5a412fcb3184d5691e20dc1f034ad2
MercedesSilva <107565943+MercedesSilva@users.noreply.github.com>
2022-08-14 19:58:42 -0300

    Update SQLEjercicioValfinal.sql

--- SQLEjercicioValfinal.sql
+++ SQLEjercicioValfinal.sql
@@ -30,6 +30,12 @@ constraint pk_cidh primary key (ID_Historial),
 constraint fk_fp foreign key(ID_Perro) references Perro (ID)
 );
 insert into Perro values (1, "Negra", 16/12/2021, "Masculino", 23002778);
-insert into Dueno values (23002778, "Juan", "Perez", 3546452317, "salta 235");
+insert into Perro values (2, "Pupy", 10/02/2022, "Femenino", 22025706);
+insert into Dueno values (1, "Juan", "Perez", 3546452317, "salta 235");
+insert into Dueno values (2, "Adriana", "Sanchez", 3546422017, "roma 22");
+
+insert into Historial values (1, 12/08/2022, 1, "cirugia miembro derecho", 3000);
+insert into Historial values (2, 13/08/2022, 2, "limpieza cuerpo", 1500);
+
 
 
