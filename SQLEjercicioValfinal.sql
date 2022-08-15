c2b6c57e8d970d4294f461f0a0b6b6ea31b8c362
MercedesSilva <107565943+MercedesSilva@users.noreply.github.com>
2022-08-15 01:56:35 -0300

    Update SQLEjercicioValfinal.sql

--- SQLEjercicioValfinal.sql
+++ SQLEjercicioValfinal.sql
@@ -1,45 +1,42 @@
-create database peluqueria canina
-use peluqueria canina; 
+create database peluqueriaCanina4
 
 --tabla  dueno
 create table Dueno(
-DNI int (11),
+DNI int,
 Nombre varchar (30),
 Apellido varchar (30),
-Telefono int,
+Telefono varchar,
 Direccion varchar (30),
 constraint pk_dni primary key (DNI)
 )
 /*tabla Perro*/
 create table Perro(
-ID_Perro varchar(10),
+ID_Perro int,
 Nombre varchar (30),
-Fecha_nac date,
+Fecha_nac datetime2,
 Sexo varchar (30),
 DNI_dueno int,
 constraint pk_cidp primary key (ID_Perro),
 constraint fk_fdni foreign key(DNI_dueno) references Dueno (DNI)
 )
 create table Historial(
-ID_Historial varchar(10),
-Fecha date,
-Perro varchar,
+ID_Historial int,
+Fecha datetime2,
+Perro int,
 Descripcion Varchar,
 Monto int,
 constraint pk_cidh primary key (ID_Historial),
-constraint fk_fp foreign key(ID_Perro) references Perro (ID)
+constraint fk_fp foreign key(Perro) references Perro (ID_Perro)
 );
-insert into Perro values (1, "Negra", 16/12/2021, "Masculino", 23002778);
-insert into Perro values (2, "Pupy", 10/02/2022, "Femenino", 22025706);
-insert into Dueno values (1, "Juan", "Perez", 3546452317, "salta 235");
-insert into Dueno values (2, "Adriana", "Sanchez", 3546422017, "roma 22");
 
-insert into Historial values (1, 12/08/2022, 1, "cirugia miembro derecho", 3000);
-insert into Historial values (2, 13/08/2022, 2, "limpieza cuerpo", 1500);
+insert into Perro values (1, 'Negra', '2012-02-21T18:10:00', 'Masculino', 23002778);
+insert into Perro values (2, 'Pupy', '2012-02-21T18:10:00', 'Femenino', 22025706);
+insert into Dueno values (1234, 'Juan', 'Perez', 3546452317, 'salta 235');
+insert into Dueno values (2345, 'Adriana', 'Sanchez',354649990022017, 'roma 22');
+
+insert into Historial values (1,'2012-02-21T18:10:00', 1, 'cirugia miembro derecho', 3000);
+insert into Historial values (2,'2012-02-21T18:10:00', 2, 'limpieza cuerpo', 1500);
 
 /* ejercicio 10 sql*/
 truncate table Historial
 DBCC CHECKIDENT(Historial, reseed, 0)
-
-
-
