b5f3170e90169a1389233517cc2676a3832824b3
MercedesSilva <107565943+MercedesSilva@users.noreply.github.com>
2022-08-15 02:11:25 -0300

    Update SQLEjercicioValfinal.sql

--- SQLEjercicioValfinal.sql
+++ SQLEjercicioValfinal.sql
@@ -1,17 +1,17 @@
-create database peluqueriaCanina4
+create database peluqueriaCanina
 
 --tabla  dueno
 create table Dueno(
 DNI int,
 Nombre varchar (30),
 Apellido varchar (30),
-Telefono varchar,
+Telefono varchar (30),
 Direccion varchar (30),
 constraint pk_dni primary key (DNI)
 )
 /*tabla Perro*/
 create table Perro(
-ID_Perro int,
+ID_Perro int IDENTITY(1,1),
 Nombre varchar (30),
 Fecha_nac datetime2,
 Sexo varchar (30),
@@ -20,22 +20,23 @@ constraint pk_cidp primary key (ID_Perro),
 constraint fk_fdni foreign key(DNI_dueno) references Dueno (DNI)
 )
 create table Historial(
-ID_Historial int,
+ID_Historial int IDENTITY(1,1),
 Fecha datetime2,
 Perro int,
-Descripcion Varchar,
+Descripcion Varchar(50),
 Monto int,
 constraint pk_cidh primary key (ID_Historial),
 constraint fk_fp foreign key(Perro) references Perro (ID_Perro)
 );
 
-insert into Perro values (1, 'Negra', '2012-02-21T18:10:00', 'Masculino', 23002778);
-insert into Perro values (2, 'Pupy', '2012-02-21T18:10:00', 'Femenino', 22025706);
-insert into Dueno values (1234, 'Juan', 'Perez', 3546452317, 'salta 235');
-insert into Dueno values (2345, 'Adriana', 'Sanchez',354649990022017, 'roma 22');
+insert into Dueno values (1234, 'Juan', 'Perez', 1235, 'salta 235');
+insert into Dueno values (2345, 'Adriana', 'Sanchez',6789, 'roma 22');
 
-insert into Historial values (1,'2012-02-21T18:10:00', 1, 'cirugia miembro derecho', 3000);
-insert into Historial values (2,'2012-02-21T18:10:00', 2, 'limpieza cuerpo', 1500);
+insert into Perro values ('Negra', '2012-02-21T18:10:00', 'Masculino', 1234);
+insert into Perro values ('Pupy', '2012-02-21T18:10:00', 'Femenino', 2345);
+
+insert into Historial values ('2012-02-21T18:10:00', 1, 'cirugia miembro derecho', 3000);
+insert into Historial values ('2012-02-21T18:10:00', 2, 'limpieza cuerpo', 1500);
 
 /* ejercicio 10 sql*/
 truncate table Historial
